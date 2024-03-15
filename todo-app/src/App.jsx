ae5194cd0248a59f141bb51e051ae4f312417a74
Hemant_programer <32216684+rathorehemant@users.noreply.github.com>
2024-03-15 19:19:48 +0530

    api code

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,34 +6,103 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
-  const[inputValue,setInputValue] = useState('');
+  const [items, setItems] = useState([]);
+  const [userValues, setUserValues] = useState('');
+  
+
+  const[inputValue,setInputValue] = useState({
+    email : '',
+    password : ''
+  });
   const [error, setError] = useState('');
-  const [previous, previousValue] = useState([]);
+  // const [previous, previousValue] = useState([]);
 
   function formSubmit(e){
     e.preventDefault();
     if(validateInput(inputValue)){
-      previousValue(previous => [...previous,inputValue])
+
+     
+      fetch('https://doshipms.com/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'NN_api_key' : 'fcbf2fee98cee0be1da58b8a2066b7d3826f335def2261d693208e16275dde5b'
+        },
+        body: JSON.stringify(inputValue)
+      })
+      .then(response => response.json())
+      .then(json => {
+        if(json.error){
+          setError(json.message)
+        }else{
+          
+    
+          let userData = {
+            'name' : json.username,
+            'user_roles' : json.user_roles,
+            'token' : json.token
+          }
+          
+          localStorage.setItem('userData', JSON.stringify(userData));
+          let UserValues = JSON.parse(localStorage.getItem('userData'))
+    
+          setUserValues(UserValues)
+        
+        }
+      }
+      )
+      
+      .catch(error => console.error(error));
+      
+      
+    
+
+      // previousValue(previous => [...previous,inputValue])
       setError('');
     }else{
       setError('Value are empty')
     }
+
+   
+      
+   
   }
 
-  const handleChange = (event)=>{
-    setInputValue(event.target.value)
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputValue(prevState => ({
+      ...prevState,
+      [name]: value
+
+    }));
+  };
+
+  
+  
+
+  const validateInput = (values)=>{
+    return values.email.trim() !== '' && values.password.trim() !== '';
+    // return values.name.trim() !== '' && values.password.trim() !==''
   }
 
-  const validateInput = (value)=>{
-    return value.trim() !== ''
-  }
+
+ 
+ 
+  
+  
+
   return (
     <>
     
       <form onSubmit={formSubmit}>
       <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="text" value = {inputValue} onChange={handleChange}className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        <label htmlFor="Email" className="form-label">Email address</label>
+        <input type="email" value = {inputValue.name} onChange={handleChange}className="form-control" id="email"  name = 'email' aria-describedby="emailHelp"/>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">Password</label>
+        <input type="password" value = {inputValue.password} onChange={handleChange}className="form-control" id="password"  name = "password" aria-describedby="emailHelp"/>
       </div>
      
       <button type="submit" className="btn btn-primary">Submit</button>
@@ -42,15 +111,13 @@ function App() {
     </form>
     
     <div>
-      <h2>Previous Values</h2>
+      <h2>User Data </h2>
       
       
-        {previous.map((value,index)=>(
-          <div key={index} className="alert alert-success" role="alert">
-          {value}
-        </div>
+       
           
-        ))}
+        <div>{userValues.name}</div>
+        <div>{userValues.token}</div>
         
     </div>
       
